3c16455cc9947acb80b4dfc39e15cc4b8a13d583
patrykSidoruk <sidoruk.ps@gmail.com>
2017-12-09 17:57:45 +0100

    Create empty tables in database, upload dump of it

--- ProgrammingSchool.sql
+++ ProgrammingSchool.sql
@@ -14,6 +14,113 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Excercise`
+--
+
+DROP TABLE IF EXISTS `Excercise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Excercise` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_polish_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Excercise`
+--
+
+LOCK TABLES `Excercise` WRITE;
+/*!40000 ALTER TABLE `Excercise` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Excercise` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Solution`
+--
+
+DROP TABLE IF EXISTS `Solution`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Solution` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `description` text COLLATE utf8_polish_ci,
+  `excercise_id` int(11) DEFAULT NULL,
+  `users_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `excercise_id` (`excercise_id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `Solution_ibfk_1` FOREIGN KEY (`excercise_id`) REFERENCES `Excercise` (`id`),
+  CONSTRAINT `Solution_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Solution`
+--
+
+LOCK TABLES `Solution` WRITE;
+/*!40000 ALTER TABLE `Solution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Solution` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User_group`
+--
+
+DROP TABLE IF EXISTS `User_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User_group`
+--
+
+LOCK TABLES `User_group` WRITE;
+/*!40000 ALTER TABLE `User_group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `User_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  `password` varchar(245) COLLATE utf8_polish_ci DEFAULT NULL,
+  `person_group_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `person_group_id` (`person_group_id`),
+  CONSTRAINT `Users_ibfk_1` FOREIGN KEY (`person_group_id`) REFERENCES `User_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +131,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-08 14:31:28
+-- Dump completed on 2017-12-09 17:53:33
